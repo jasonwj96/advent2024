53370d8813beac683655d798b6bae36cdfda914a
Jason Wedderburn <jasonwj96@gmail.com>
2024-12-03 02:59:26 -0500

    Finished puzzle 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate itertools;
+extern crate nalgebra;
 
 mod solutions;
 
 fn main() {
     println!("Puzzle 1, Part 1: {}", solutions::p01::part1());
-    println!("Puzzle 2, Part 2: {}", solutions::p01::part2());
+    println!("Puzzle 1, Part 2: {}", solutions::p01::part2());
+    println!("Puzzle 2, Part 1: {}", solutions::p02::part1());
+    println!("Puzzle 2, Part 2: {}", solutions::p02::part2());
 }
