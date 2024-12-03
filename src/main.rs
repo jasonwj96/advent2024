a1a0c8114d79109a96c58fbdba144c681299b20d
Jason Wedderburn <jasonwj96@gmail.com>
2024-12-03 11:16:39 -0500

    Finished puzzle 3 part 1

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,6 @@ fn main() {
     println!("Puzzle 1, Part 2: {}", solutions::p01::part2());
     println!("Puzzle 2, Part 1: {}", solutions::p02::part1());
     println!("Puzzle 2, Part 2: {}", solutions::p02::part2());
+    println!("Puzzle 3, Part 1: {}", solutions::p03::part1());
+    println!("Puzzle 3, Part 2: {}", solutions::p03::part2());
 }
