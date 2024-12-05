33a6f6d5db98f6a8414703e2e8131a85b3ee6f1b
Jason Wedderburn <jasonwj96@gmail.com>
2024-12-05 03:50:30 -0500

    Extract puzzle 5 data

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ mod solutions;
 mod util;
 
 fn main() {
-    /*
-      println!("Puzzle 1, Part 1: {}", solutions::p01::part1());
-      println!("Puzzle 1, Part 2: {}", solutions::p01::part2());
-      println!("Puzzle 2, Part 1: {}", solutions::p02::part1());
-      println!("Puzzle 2, Part 2: {}", solutions::p02::part2());
-      println!("Puzzle 3, Part 1: {}", solutions::p03::part1());
-      println!("Puzzle 3, Part 2: {}", solutions::p03::part2());
-      */
-
+    println!("Puzzle 1, Part 1: {}", solutions::p01::part1());
+    println!("Puzzle 1, Part 2: {}", solutions::p01::part2());
+    println!("Puzzle 2, Part 1: {}", solutions::p02::part1());
+    println!("Puzzle 2, Part 2: {}", solutions::p02::part2());
+    println!("Puzzle 3, Part 1: {}", solutions::p03::part1());
+    println!("Puzzle 3, Part 2: {}", solutions::p03::part2());
     println!("Puzzle 4, Part 1: {}", solutions::p04::part1());
     println!("Puzzle 4, Part 2: {}", solutions::p04::part2());
+    println!("Puzzle 5, Part 1: {}", solutions::p05::part1());
+    println!("Puzzle 5, Part 2: {}", solutions::p05::part2());
 }
