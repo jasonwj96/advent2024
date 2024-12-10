e7abbcd2a122127a178eb948960d8d883f1b9b49
Jason Wedderburn <jasonwj96@gmail.com>
2024-12-10 05:06:37 -0500

    Imported another project

---
+++ src/lib.rs
@@ -0,0 +1,304 @@
+//! # Advent of Code solutions in Rust, tuned for speed.
+//!
+//! [![badge]][link]
+//!
+//! [badge]: https://img.shields.io/badge/github-blue?style=for-the-badge&logo=github&labelColor=grey
+//! [link]: https://github.com/maneatingape/advent-of-code-rust
+
+// Portable SIMD API is enabled by "simd" feature.
+#![cfg_attr(feature = "simd", allow(unstable_features), feature(portable_simd))]
+// Configure rustdoc.
+#![doc(html_logo_url = "https://maneatingape.github.io/advent-of-code-rust/logo.png")]
+
+/// # Utility modules to handle common recurring Advent of Code patterns.
+pub mod util {
+    pub mod ansi;
+    pub mod bitset;
+    pub mod grid;
+    pub mod hash;
+    pub mod heap;
+    pub mod integer;
+    pub mod iter;
+    pub mod math;
+    pub mod md5;
+    pub mod parse;
+    pub mod point;
+    pub mod slice;
+    pub mod thread;
+}
+
+/// # Help Santa by solving puzzles to fix the weather machine's snow function.
+pub mod year2015 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+    pub mod day11;
+    pub mod day12;
+    pub mod day13;
+    pub mod day14;
+    pub mod day15;
+    pub mod day16;
+    pub mod day17;
+    pub mod day18;
+    pub mod day19;
+    pub mod day20;
+    pub mod day21;
+    pub mod day22;
+    pub mod day23;
+    pub mod day24;
+    pub mod day25;
+}
+
+/// # Defeat the Easter Bunny to save Christmas.
+pub mod year2016 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+    pub mod day11;
+    pub mod day12;
+    pub mod day13;
+    pub mod day14;
+    pub mod day15;
+    pub mod day16;
+    pub mod day17;
+    pub mod day18;
+    pub mod day19;
+    pub mod day20;
+    pub mod day21;
+    pub mod day22;
+    pub mod day23;
+    pub mod day24;
+    pub mod day25;
+}
+
+/// # A technical support callout from the Elves escalates rapidly.
+pub mod year2017 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+    pub mod day11;
+    pub mod day12;
+    pub mod day13;
+    pub mod day14;
+    pub mod day15;
+    pub mod day16;
+    pub mod day17;
+    pub mod day18;
+    pub mod day19;
+    pub mod day20;
+    pub mod day21;
+    pub mod day22;
+    pub mod day23;
+    pub mod day24;
+    pub mod day25;
+}
+
+/// # Travel through time to restore the festive timeline.
+pub mod year2018 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+    pub mod day11;
+    pub mod day12;
+    pub mod day13;
+    pub mod day14;
+    pub mod day15;
+    pub mod day16;
+    pub mod day17;
+    pub mod day18;
+    pub mod day19;
+    pub mod day20;
+    pub mod day21;
+    pub mod day22;
+    pub mod day23;
+    pub mod day24;
+    pub mod day25;
+}
+
+/// # Rescue Santa from deep space with a solar system voyage.
+pub mod year2019 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+    pub mod day11;
+    pub mod day12;
+    pub mod day13;
+    pub mod day14;
+    pub mod day15;
+    pub mod day16;
+    pub mod day17;
+    pub mod day18;
+    pub mod day19;
+    pub mod day20;
+    pub mod day21;
+    pub mod day22;
+    pub mod day23;
+    pub mod day24;
+    pub mod day25;
+    pub mod intcode;
+}
+
+/// # What could go wrong trying to enjoy a well deserved vacation?
+pub mod year2020 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+    pub mod day11;
+    pub mod day12;
+    pub mod day13;
+    pub mod day14;
+    pub mod day15;
+    pub mod day16;
+    pub mod day17;
+    pub mod day18;
+    pub mod day19;
+    pub mod day20;
+    pub mod day21;
+    pub mod day22;
+    pub mod day23;
+    pub mod day24;
+    pub mod day25;
+}
+
+/// # Retrieve the keys to Santa's sleigh with an underwater submarine adventure.
+pub mod year2021 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+    pub mod day11;
+    pub mod day12;
+    pub mod day13;
+    pub mod day14;
+    pub mod day15;
+    pub mod day16;
+    pub mod day17;
+    pub mod day18;
+    pub mod day19;
+    pub mod day20;
+    pub mod day21;
+    pub mod day22;
+    pub mod day23;
+    pub mod day24;
+    pub mod day25;
+}
+
+/// # Assist the Elves on their annual jungle expedition.
+pub mod year2022 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+    pub mod day11;
+    pub mod day12;
+    pub mod day13;
+    pub mod day14;
+    pub mod day15;
+    pub mod day16;
+    pub mod day17;
+    pub mod day18;
+    pub mod day19;
+    pub mod day20;
+    pub mod day21;
+    pub mod day22;
+    pub mod day23;
+    pub mod day24;
+    pub mod day25;
+}
+
+/// # Restore global snow production.
+pub mod year2023 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+    pub mod day11;
+    pub mod day12;
+    pub mod day13;
+    pub mod day14;
+    pub mod day15;
+    pub mod day16;
+    pub mod day17;
+    pub mod day18;
+    pub mod day19;
+    pub mod day20;
+    pub mod day21;
+    pub mod day22;
+    pub mod day23;
+    pub mod day24;
+    pub mod day25;
+}
+
+/// # Locate the Chief Historian in time for the big Christmas sleigh launch.
+pub mod year2024 {
+    pub mod day01;
+    pub mod day02;
+    pub mod day03;
+    pub mod day04;
+    pub mod day05;
+    pub mod day06;
+    pub mod day07;
+    pub mod day08;
+    pub mod day09;
+    pub mod day10;
+}
---
+++ tests/test.rs
@@ -0,0 +1,294 @@
+// Templates
+
+// pub fn parse(_input: &str) -> Vec<u32> {
+//     vec![]
+// }
+//
+// pub fn part1(_input: &[u32]) -> u32 {
+//     123
+// }
+//
+// pub fn part2(_input: &[u32]) -> u32 {
+//     456
+// }
+
+// use aoc::year2024::day00::*;
+//
+// const EXAMPLE: &str = "";
+//
+// #[test]
+// fn part1_test() {
+//     let input = parse(EXAMPLE);
+//     assert_eq!(part1(&input), 123);
+// }
+//
+// #[test]
+// fn part2_test() {
+//     let input = parse(EXAMPLE);
+//     assert_eq!(part2(&input), 456);
+// }
+
+mod year2015 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+    mod day11_test;
+    mod day12_test;
+    mod day13_test;
+    mod day14_test;
+    mod day15_test;
+    mod day16_test;
+    mod day17_test;
+    mod day18_test;
+    mod day19_test;
+    mod day20_test;
+    mod day21_test;
+    mod day22_test;
+    mod day23_test;
+    mod day24_test;
+    mod day25_test;
+}
+
+mod year2016 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+    mod day11_test;
+    mod day12_test;
+    mod day13_test;
+    mod day14_test;
+    mod day15_test;
+    mod day16_test;
+    mod day17_test;
+    mod day18_test;
+    mod day19_test;
+    mod day20_test;
+    mod day21_test;
+    mod day22_test;
+    mod day23_test;
+    mod day24_test;
+    mod day25_test;
+}
+
+mod year2017 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+    mod day11_test;
+    mod day12_test;
+    mod day13_test;
+    mod day14_test;
+    mod day15_test;
+    mod day16_test;
+    mod day17_test;
+    mod day18_test;
+    mod day19_test;
+    mod day20_test;
+    mod day21_test;
+    mod day22_test;
+    mod day23_test;
+    mod day24_test;
+    mod day25_test;
+}
+
+mod year2018 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+    mod day11_test;
+    mod day12_test;
+    mod day13_test;
+    mod day14_test;
+    mod day15_test;
+    mod day16_test;
+    mod day17_test;
+    mod day18_test;
+    mod day19_test;
+    mod day20_test;
+    mod day21_test;
+    mod day22_test;
+    mod day23_test;
+    mod day24_test;
+    mod day25_test;
+}
+
+mod year2019 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+    mod day11_test;
+    mod day12_test;
+    mod day13_test;
+    mod day14_test;
+    mod day15_test;
+    mod day16_test;
+    mod day17_test;
+    mod day18_test;
+    mod day19_test;
+    mod day20_test;
+    mod day21_test;
+    mod day22_test;
+    mod day23_test;
+    mod day24_test;
+    mod day25_test;
+}
+
+mod year2020 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+    mod day11_test;
+    mod day12_test;
+    mod day13_test;
+    mod day14_test;
+    mod day15_test;
+    mod day16_test;
+    mod day17_test;
+    mod day18_test;
+    mod day19_test;
+    mod day20_test;
+    mod day21_test;
+    mod day22_test;
+    mod day23_test;
+    mod day24_test;
+    mod day25_test;
+}
+
+mod year2021 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+    mod day11_test;
+    mod day12_test;
+    mod day13_test;
+    mod day14_test;
+    mod day15_test;
+    mod day16_test;
+    mod day17_test;
+    mod day18_test;
+    mod day19_test;
+    mod day20_test;
+    mod day21_test;
+    mod day22_test;
+    mod day23_test;
+    mod day24_test;
+    mod day25_test;
+}
+
+mod year2022 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+    mod day11_test;
+    mod day12_test;
+    mod day13_test;
+    mod day14_test;
+    mod day15_test;
+    mod day16_test;
+    mod day17_test;
+    mod day18_test;
+    mod day19_test;
+    mod day20_test;
+    mod day21_test;
+    mod day22_test;
+    mod day23_test;
+    mod day24_test;
+    mod day25_test;
+}
+
+mod year2023 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+    mod day11_test;
+    mod day12_test;
+    mod day13_test;
+    mod day14_test;
+    mod day15_test;
+    mod day16_test;
+    mod day17_test;
+    mod day18_test;
+    mod day19_test;
+    mod day20_test;
+    mod day21_test;
+    mod day22_test;
+    mod day23_test;
+    mod day24_test;
+    mod day25_test;
+}
+
+mod year2024 {
+    mod day01_test;
+    mod day02_test;
+    mod day03_test;
+    mod day04_test;
+    mod day05_test;
+    mod day06_test;
+    mod day07_test;
+    mod day08_test;
+    mod day09_test;
+    mod day10_test;
+}
